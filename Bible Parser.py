b0d775be38cdddb5e3b7941e5acc20aff70d93e6
Messinground <90413172+Messinground@users.noreply.github.com>
2021-09-10 11:18:13 -0500

    Added Parser file

---
+++ Bible Parser.py
@@ -0,0 +1,63 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import os
+import re
+
+Biburl = urllib.request.urlopen('https://bereanbible.com/bsb.txt')
+BibleText = str(Biburl.read()).replace('\\t','\t').split('\\n')
+
+# Or if you are parsing from a text file:
+# BibleText = open('bib.txt.', encoding="utf8").read().split('\n')
+os.system("pause")
+
+print('Type each item you would like to search for, pressing "enter" after each. Type "Done" when you are finished.')
+needle = []
+while 'Done' not in needle:
+    needle.append(input())
+needle = needle[0:len(needle)-1]
+
+Counts = 0
+References = {}
+
+for line in BibleText:
+    LineWords = line.split()
+    if len(LineWords) >= 2:
+        ThisRef = LineWords[0], LineWords[1]
+
+    for Each in needle:
+        while Each in line:
+            Counts = Counts + 1
+            References[Counts] = ThisRef
+
+            # The following code will:
+            # - Find the length of the needle
+            # - Find out where the needle is in the line
+            # - Remove the needle from the line (and any extra space)
+            EachLen = len(Each)
+            EachPos = line.find(Each)
+            line = line[:EachPos].rstrip() + line[EachPos+EachLen:]
+          
+
+print(References)
+print('total:',Counts)
+
+"""
+    # old code - This checks each item word by word rather than phrase by phrase
+    for Each in needle:
+        if Each in line:
+            for word in LineWords:
+                # print('checking', word)
+                if Each in word:
+                    Counts = Counts + 1
+                    References[Counts] = ThisRef
+                    # print('FOUND:',word)
+            # print('Total',Counts, 'as of', ThisRef)
+            # os.system("pause")
+"""   
+    
+    
+    
+
+    
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BibParser
+
+This is a python-based practice project made to parse phrases and words from the Bible
